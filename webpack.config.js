1b6b0f71360a2cfd4521578ba0b238bf5d1d7158
jerry.zhou <jiahuasir@163.com>
2023-04-04 02:22:47 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -39,14 +39,18 @@ module.exports = {
   plugins: [
     new CopyWebpackPlugin({
       patterns: [
+        // {
+        //   from: path.resolve(__dirname, 'src/public'), // 源文件夹路径
+        //   to: path.resolve(__dirname, 'dist/public'), // 目标文件夹路径
+        // },
+        // {
+        //   from: path.resolve(__dirname, 'src/api-docs'), // 源文件夹路径
+        //   to: path.resolve(__dirname, 'dist/api-docs'), // 目标文件夹路径
+        // },
         {
-          from: path.resolve(__dirname, 'src/public'), // 源文件夹路径
-          to: path.resolve(__dirname, 'dist/public'), // 目标文件夹路径
+          from: path.resolve(__dirname, 'src'), // 源文件夹路径
+          to: path.resolve(__dirname, 'dist'), // 目标文件夹路径
         },
-        {
-          from: path.resolve(__dirname, 'src/api-docs'), // 源文件夹路径
-          to: path.resolve(__dirname, 'dist/api-docs'), // 目标文件夹路径
-        }
       ],
     }),
   ],
