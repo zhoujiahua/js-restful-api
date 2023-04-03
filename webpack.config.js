fcfa7e42113973cd14e9baf20611fd520094c52a
jerry.zhou <jiahuasir@163.com>
2023-04-04 00:03:43 +0800

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'app.js',
   },
   target: 'node',
   module: {
